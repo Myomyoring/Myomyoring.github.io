c422bf18367bdc6040ae1add8fa4879db3e7894e
Myomyoring <narcissuscat@naver.com>
2023-11-19 14:17:43 +0900

    chore: migration 'Gatsby'

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,86 +1,83 @@
-import type { GatsbyConfig, PluginRef } from "gatsby"
-import "dotenv/config"
+import type { GatsbyConfig, PluginRef } from 'gatsby';
+import 'dotenv/config';
 
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // You can also add new values here to query them like usual
-    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
-    siteImage: `/banner.jpg`,
-    siteLanguage: `en`,
-    author: `@lekoarts_de`,
-  },
-  trailingSlash: `always`,
-  plugins: [
-    {
-      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
-      // See the theme's README for all available options
-      options: {
-        navigation: [
-          {
-            title: `Blog`,
-            slug: `/blog`,
-          },
-          {
-            title: `About`,
-            slug: `/about`,
-          },
-        ],
-        externalLinks: [
-          {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-sitemap`,
-      options: {
-        output: `/`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
-        start_url: `/`,
-        background_color: `#fff`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#6B46C1`,
-        display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
+	siteMetadata: {
+		siteTitle: `Myolog`,
+		siteTitleAlt: `Myolog | Tech Blog`,
+		siteHeadline: `Myolog | Tech Blog`,
+		siteUrl: `https://myomyoring.github.io/`,
+		siteDescription: `프론트엔드 개발자 취준생 기술 블로그`,
+		siteImage: `/banner.jpg`,
+		siteLanguage: `ko`,
+		author: `@Myomyoring`,
+	},
+	trailingSlash: `always`,
+	plugins: [
+		{
+			resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+			options: {
+				navigation: [
+					{
+						title: `Blog`,
+						slug: `/blog`,
+					},
+					{
+						title: `About`,
+						slug: `/about`,
+					},
+				],
+				externalLinks: [
+					{
+						name: `Github`,
+						url: `https://github.com/Myomyoring`,
+					},
+				],
+			},
+		},
+		{
+			resolve: `gatsby-plugin-robots-txt`,
+			options: {
+				host: `https://myomyoring.github.io/`,
+				sitemap: `https://myomyoring.github.io/sitemap.xml`,
+				policy: [{ userAgent: '*', allow: '/' }],
+			},
+		},
+		{
+			resolve: `gatsby-plugin-sitemap`,
+			options: {
+				output: `/`,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Myolog | Tech Blog`,
+				short_name: `Myolog`,
+				description: `프론트엔드 개발자 취준생 기술 블로그`,
+				start_url: `/`,
+				background_color: `#fff`,
+				display: `standalone`,
+				icons: [
+					{
+						src: `/android-chrome-192x192.png`,
+						sizes: `192x192`,
+						type: `image/png`,
+					},
+					{
+						src: `/android-chrome-512x512.png`,
+						sizes: `512x512`,
+						type: `image/png`,
+					},
+				],
+			},
+		},
+		{
+			resolve: `gatsby-plugin-feed`,
+			options: {
+				query: `
           {
             site {
               siteMetadata {
@@ -92,87 +89,87 @@ const config: GatsbyConfig = {
             }
           }
         `,
-        feeds: [
-          {
-            serialize: ({
-              query: { site, allPost },
-            }: {
-              query: { allPost: IAllPost; site: { siteMetadata: ISiteMetadata } }
-            }) =>
-              allPost.nodes.map((post) => {
-                const url = site.siteMetadata.siteUrl + post.slug
-                const content = `<p>${post.excerpt}</p><div style="margin-top: 50px; font-style: italic;"><strong><a href="${url}">Keep reading</a>.</strong></div><br /> <br />`
+				feeds: [
+					{
+						serialize: ({
+							query: { site, allPost },
+						}: {
+							query: { allPost: IAllPost; site: { siteMetadata: ISiteMetadata } };
+						}) =>
+							allPost.nodes.map((post) => {
+								const url = site.siteMetadata.siteUrl + post.slug;
+								const content = `<p>${post.excerpt}</p><div style="margin-top: 50px; font-style: italic;"><strong><a href="${url}">Keep reading</a>.</strong></div><br /> <br />`;
 
-                return {
-                  title: post.title,
-                  date: post.date,
-                  excerpt: post.excerpt,
-                  url,
-                  guid: url,
-                  custom_elements: [{ "content:encoded": content }],
-                }
-              }),
-            query: `{
+								return {
+									title: post.title,
+									date: post.date,
+									excerpt: post.excerpt,
+									url,
+									guid: url,
+									custom_elements: [{ 'content:encoded': content }],
+								};
+							}),
+						query: `{
   allPost(sort: {date: DESC}) {
     nodes {
       title
-      date(formatString: "MMMM D, YYYY")
+      date(formatString: "YYYY년 MM월 D일")
       excerpt
       slug
     }
   }
 }`,
-            output: `rss.xml`,
-            title: `Minimal Blog - @lekoarts/gatsby-theme-minimal-blog`,
-          },
-        ],
-      },
-    },
-    // You can remove this plugin if you don't need it
-    shouldAnalyseBundle && {
-      resolve: `gatsby-plugin-webpack-statoscope`,
-      options: {
-        saveReportTo: `${__dirname}/public/.statoscope/_bundle.html`,
-        saveStatsTo: `${__dirname}/public/.statoscope/_stats.json`,
-        open: false,
-      },
-    },
-  ].filter(Boolean) as Array<PluginRef>,
-}
+						output: `rss.xml`,
+						title: `Myolog`,
+					},
+				],
+			},
+		},
+		// You can remove this plugin if you don't need it
+		shouldAnalyseBundle && {
+			resolve: `gatsby-plugin-webpack-statoscope`,
+			options: {
+				saveReportTo: `${__dirname}/public/.statoscope/_bundle.html`,
+				saveStatsTo: `${__dirname}/public/.statoscope/_stats.json`,
+				open: false,
+			},
+		},
+	].filter(Boolean) as Array<PluginRef>,
+};
 
-export default config
+export default config;
 
 interface IPostTag {
-  name: string
-  slug: string
+	name: string;
+	slug: string;
 }
 
 interface IPost {
-  slug: string
-  title: string
-  defer: boolean
-  date: string
-  excerpt: string
-  contentFilePath: string
-  html: string
-  timeToRead: number
-  wordCount: number
-  tags: Array<IPostTag>
-  banner: any
-  description: string
-  canonicalUrl: string
+	slug: string;
+	title: string;
+	defer: boolean;
+	date: string;
+	excerpt: string;
+	contentFilePath: string;
+	html: string;
+	timeToRead: number;
+	wordCount: number;
+	tags: Array<IPostTag>;
+	banner: any;
+	description: string;
+	canonicalUrl: string;
 }
 
 interface IAllPost {
-  nodes: Array<IPost>
+	nodes: Array<IPost>;
 }
 
 interface ISiteMetadata {
-  siteTitle: string
-  siteTitleAlt: string
-  siteHeadline: string
-  siteUrl: string
-  siteDescription: string
-  siteImage: string
-  author: string
+	siteTitle: string;
+	siteTitleAlt: string;
+	siteHeadline: string;
+	siteUrl: string;
+	siteDescription: string;
+	siteImage: string;
+	author: string;
 }
